e1b3e1e05053757d152b4e0b51dfd55dd2f06f11
israel brian (ses) <zdesenhos@gmail.com>
2025-06-13 15:46:49 -0300

    logica geração de assinatura completamente feita

--- app.py
+++ app.py
@@ -12,16 +12,19 @@ def gerar_assinatura():
     # cargo = "Técnico em Informática - MGS"
     cargo = "Estagiario"
     orgao = "ASSESSORIA DE TECNOLOGIA DA INFORMAÇÃO" 
-    # orgao = "SECRETARIA DE ESTADO DE SAÚDE" 
-    telefone_fixo = "(31) 3277-0000"
-    telefone_celular = "(31) 99999-9999"  # Este é opcional
-    email = "nome.servidor@email.gov.br"
-    endereco = "Av. Afonso Pena, 1212"
-    andar = "10º andar"
+    telefone_fixo = "(31) 3916 0031"
+    # telefone_celular = "(31) 99999-9999"  # Este é opcional
+    email = "israel.goncalves@saude.mg.gov.br"
+    andar = "12º"
+    endereco = f'Cidade Administrativa, Prédio Minas,{andar} andar'
 
     cord_nome = (55, 78)
     cord_cargo = (55, 134)
     cord_orgao = (56, 168)
+    cord_tel_fixo = (90, 286)
+    # cord_tel_cel = (90, 290)
+    cord_email = (90, 325)
+    cord_end = (90, 370)
 
     try:
         # Abre o template da imagem
@@ -37,10 +40,12 @@ def gerar_assinatura():
         # CRIAR PADRÃO PRO NOME - NEGRITO - 16 - ROXO
         fontNome = ImageFont.truetype(caminho_fonte_negrito1, 22)
         # CRIAR PADRÃO CARGO - NORMAL - 16 - ROXO
-        fontCargo = ImageFont.truetype(caminho_fonte, 20)
+        fontPadraoGG = ImageFont.truetype(caminho_fonte, 22)
+        fontPadraoG = ImageFont.truetype(caminho_fonte, 20)
         # CRIAR PADRÃO ORGAO - 12 - AMARELO - SEMPRE MAIUSCULO
         fontOrgao = ImageFont.truetype(caminho_fonte_nb, 17)
         texto_roxo = (131,35,112)  
+        texto_roxo_claro = (137,71,118)  
         texto_laranja = (244,148,60)  
         # Criando camada de desenho sobre a imagem\
         desenho = ImageDraw.Draw(img)
@@ -48,8 +53,11 @@ def gerar_assinatura():
         # Adicionando os textos na imagem
         # O formato é: desenho.text((coordenada_x, coordenada_y), texto, fill=cor, font=fonte)
         desenho.text((cord_nome), nome, font=fontNome, fill=texto_roxo)
-        desenho.text((cord_cargo), cargo, font=fontCargo, fill=texto_roxo)
+        desenho.text((cord_cargo), cargo, font=fontPadraoGG, fill=texto_roxo)
         desenho.text((cord_orgao), orgao, font=fontOrgao, fill=texto_laranja)
+        desenho.text((cord_tel_fixo), telefone_fixo, font=fontPadraoG, fill=texto_roxo)
+        desenho.text((cord_email), email, font=fontPadraoG, fill=texto_roxo_claro)
+        desenho.text((cord_end), endereco, font=fontPadraoG, fill=texto_roxo_claro)
 
         # Salva a imagem gerada em memória
         # Usamos io.BytesIO para evitar salvar o arquivo no disco do servidor.
@@ -73,5 +81,3 @@ def gerar_assinatura():
 # Roda a aplicação em modo de depuração para facilitar o desenvolvimento
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
