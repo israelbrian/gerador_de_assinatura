43ac939adff6ceca4d8984f8999df8d85be5704a
israel <zdesenhos@gmail.com>
2025-06-18 10:51:03 -0300

    impletamentação do tailwind.css funcionando
    lembrar de usar o comando "npx tailwindcss -i ./src/input.css -o ./static/css/output.css --watch" via cmd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./templates/**/*.html", // Aponta para todos os arquivos .html dentro da pasta templates
+    "./static/js/**/*.js"   // Aponta para todos os arquivos .js dentro da pasta static/js
+  ],
   theme: {
     extend: {},
   },
