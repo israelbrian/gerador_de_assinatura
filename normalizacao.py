ece9d4526414d6c5bf005845315da8c136c32e9f
israel brian (ses) <zdesenhos@gmail.com>
2025-06-25 12:47:07 -0300

    att em normalizacao
    Foi criado o if para formatar o numero de: (3166665555) para (31 6666-5555)

--- normalizacao.py
+++ normalizacao.py
@@ -5,7 +5,10 @@ def normallizar_dados(dados_usuario):
             if chave == 'nome' or chave == 'cargo':
                 dados_usuario[chave] = valor.title().replace('De','de').replace('Da','da').replace('Das','das').replace('Do','do').replace('Dos','dos')  
             elif chave == 'orgao':
-                dados_usuario[chave] = valor.upper().replace('De','de').replace('Em','em')     
+                dados_usuario[chave] = valor.upper().replace('De','de').replace('Em','em')  
+            elif chave == 'telefone_fixo':
+                if len(valor) == 10:  # Formato 0XXXXXXXXXX
+                    dados_usuario[chave] = valor[0:2] + ' ' + valor[2:6] + '-' + valor[6:10]
             else:
                 dados_usuario[chave] = valor.lower()
         
