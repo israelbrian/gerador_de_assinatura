8e02ea8c498ff5a9b09abd0c29face0836e86b6d
israel <zdesenhos@gmail.com>
2025-06-25 11:13:48 -0300

    att normalizacao

--- normalizacao.py
+++ normalizacao.py
@@ -5,6 +5,7 @@ def normallizar_dados(dados_usuario):
             if chave == 'nome' or chave == 'cargo':
                 dados_usuario[chave] = valor.title().replace('De','de').replace('Da','da').replace('Das','das').replace('Do','do').replace('Dos','dos')  
             elif chave == 'orgao':
-                dados_usuario[chave] = valor.upper()            
+                dados_usuario[chave] = valor.upper().replace('De','de').replace('Em','em')     
             else:
                 dados_usuario[chave] = valor.lower()
+        
