156b537e9265e311a6610db4083d9be929a614ba
israel <zdesenhos@gmail.com>
2025-07-07 13:33:26 -0300

    correção dos nomes dos indices do dict usado como parametro na normalização

--- normalize.py
+++ normalize.py
@@ -3,9 +3,9 @@ def normallize_data(userData):
     for key, value in userData.items():
         if isinstance(value, str):
             if key == 'fullName' or key == 'jobTitle':
-                userData[key] = value.title().replace('De','de').replace('Da','da').replace('Das','das').replace('Do','do').replace('Dos','dos')  
+                userData[key] = value.title().replace('De','de').replace('Da','da').replace('Das','das').replace('Do','do').replace('Dos','dos').replace('Em','em')  
             elif key == 'department':
-                userData[key] = value.upper().replace('De','de').replace('Em','em')  
+                userData[key] = value.upper()
             elif key == 'phoneNumber':
                 if len(value) == 10:
                     userData[key] = value[0:2] + ' ' + value[2:6] + '-' + value[6:10]
