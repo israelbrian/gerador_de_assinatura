0e5aa1b915ed4dcf2af182415a5639adbbaf1849
israel <zdesenhos@gmail.com>
2025-07-14 16:59:05 -0300

    correção na organização das pastas correção no codigo ontem o arquivo era necessario.

--- signature_generator.py
+++ signature_generator.py
@@ -4,13 +4,13 @@ import os
 
 DIRETORIO_BASE = os.path.dirname(os.path.abspath(__file__))
 STATIC = os.path.join(DIRETORIO_BASE, 'static')
-SIGNATURE_DEFAULT = os.path.join(STATIC, 'default_signature_ses.PNG')
+SIGNATURE_DEFAULT = os.path.join(STATIC, 'images/default_signature_ses.PNG')
 FINAL_SIZE = (500, 241)
 
-FONTES = {
-    'default': os.path.join(STATIC, 'arial.ttf'),
-    'negrito': os.path.join(STATIC, 'ariblk.ttf'),
-    'semicond': os.path.join(STATIC,'arialnb.TTF')
+FONTS = {
+    'default': os.path.join(STATIC, 'fonts/arial.ttf'),
+    'negrito': os.path.join(STATIC, 'fonts/ariblk.ttf'),
+    'semicond': os.path.join(STATIC,'fonts/arialnb.TTF')
 }
 
 COORDS = {
@@ -34,28 +34,28 @@ def signatureGenerator(userData: dict) -> io.BytesIO:
     Usa o template de imagem padrão e preenche com os dados fornecidos.
     Parâmetros:
     userData (dict): Dicionário contendo os dados do usuário, como name, jobTitle, órgão, phoneNumber, email e andar.
-    Parametros pré-definidos e estaticos: coordenadas, COLORS e fontes(declarados como const no inicio do código).
+    Parametros pré-definidos e estaticos: coordenadas, COLORS e FONTS(declarados como const no inicio do código).
     Retorno: bufferMemory com a imagem gerada em formato PNG como um objeto BytesIO em memória, sem salvar no disco.
     """
     adress = f"Cidade Administrativa, Prédio Minas, {userData.get('floor', '')} andar"
 
     try:
 
-        FONTES_PIL = {
-        'name': ImageFont.truetype(FONTES['negrito'], 24),
-        'defaultGG': ImageFont.truetype(FONTES['default'], 24),
-        'defaultG': ImageFont.truetype(FONTES['default'], 22),
-        'department': ImageFont.truetype(FONTES['semicond'], 17)
+        FONTS_PIL = {
+        'name': ImageFont.truetype(FONTS['negrito'], 24),
+        'defaultGG': ImageFont.truetype(FONTS['default'], 24),
+        'defaultG': ImageFont.truetype(FONTS['default'], 22),
+        'department': ImageFont.truetype(FONTS['semicond'], 17)
         }
         img = Image.open(SIGNATURE_DEFAULT).convert("RGBA")
         desenho = ImageDraw.Draw(img)
 
-        desenho.text((COORDS['name']), userData.get('fullName', ''), font=FONTES_PIL['name'], fill=COLORS['purple'])
-        desenho.text((COORDS['jobTitle']), userData.get('jobTitle', ''), font=FONTES_PIL['defaultGG'], fill=COLORS['purple'])
-        desenho.text((COORDS['department']), userData.get('department', ''), font=FONTES_PIL['department'], fill=COLORS['orange'])
-        desenho.text((COORDS['phoneNumber']), userData.get('phoneNumber', ''), font=FONTES_PIL['defaultG'], fill=COLORS['purple'])
-        desenho.text((COORDS['email']), userData.get('email', ''), font=FONTES_PIL['defaultG'], fill=COLORS['purpleLight'])
-        desenho.text((COORDS['adress']), adress, font=FONTES_PIL['defaultG'], fill=COLORS['purpleLight'])
+        desenho.text((COORDS['name']), userData.get('fullName', ''), font=FONTS_PIL['name'], fill=COLORS['purple'])
+        desenho.text((COORDS['jobTitle']), userData.get('jobTitle', ''), font=FONTS_PIL['defaultGG'], fill=COLORS['purple'])
+        desenho.text((COORDS['department']), userData.get('department', ''), font=FONTS_PIL['department'], fill=COLORS['orange'])
+        desenho.text((COORDS['phoneNumber']), userData.get('phoneNumber', ''), font=FONTS_PIL['defaultG'], fill=COLORS['purple'])
+        desenho.text((COORDS['email']), userData.get('email', ''), font=FONTS_PIL['defaultG'], fill=COLORS['purpleLight'])
+        desenho.text((COORDS['adress']), adress, font=FONTS_PIL['defaultG'], fill=COLORS['purpleLight'])
 
         imgResized = img.resize(FINAL_SIZE, Image.Resampling.LANCZOS)
 
