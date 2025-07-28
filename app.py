3809070538f5be050f0cc1fa19d22a2f0fb8744b
israel <zdesenhos@gmail.com>
2025-07-28 06:56:07 -0300

    remoção do 'teste'

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.register_blueprint(form_route)
 app.register_blueprint(api_route)
-# teste
+
 # if __name__ == '__main__':
 #     app.run(debug=True)
 #     app.run(host='0.0.0.0', port=5000, debug=False)
