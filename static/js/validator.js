3715d1ea12f9a8b86ee1989864378516280ebf21
israel <zdesenhos@gmail.com>
2025-07-29 14:24:40 -0300

    remoção dos comentarios

--- static/js/validator.js
+++ static/js/validator.js
@@ -3,17 +3,13 @@ const inputPhoneNumber = document.getElementById('phoneNumber');
 inputPhoneNumber.addEventListener('input', (event) =>  {
   const target = event.target;
 
-  // 1. Remove tudo que não for dígito.
   let value = target.value.replace(/\D/g, '');
 
-  // 2. Limita o tamanho para 10 dígitos (telefone fixo com DDD).
   value = value.substring(0, 10);
 
-  // 3. Aplica a máscara de forma progressiva.
-  value = value.replace(/^(\d{2})(\d)/, '($1) $2'); // Adiciona parênteses e espaço após o DDD.
-  value = value.replace(/(\d{4})(\d)/, '$1-$2');   // Adiciona o hífen.
+  value = value.replace(/^(\d{2})(\d)/, '($1) $2');
+  value = value.replace(/(\d{4})(\d)/, '$1-$2');
 
-  // 4. Atualiza o valor do campo.
   target.value = value;
 });
 
