96a92382f93cff4976a70394409878a9787965db
israel <zdesenhos@gmail.com>
2025-07-29 19:24:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ app.register_blueprint(api_route)
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
-#     app.run(host='0.0.0.0', port=5000, debug=False)
+#     app.run(host='0.0.0.0', port=5000, debug=True)
