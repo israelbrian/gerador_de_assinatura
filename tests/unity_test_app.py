28e1232757b40cdd5b1d51cce88b9c15ba36db4f
israel <israel_brian@hotmail.com>
2025-12-14 00:07:42 -0300

    Att nos testes para o back

--- tests/unity_test_app.py
+++ tests/unity_test_app.py
@@ -18,4 +18,5 @@ def test_form_page_loads(client):
     response = client.get('/')
     assert response.status_code == 200
     # Verifique se um texto esperado está na página
-    assert b"Gerador de Assinatura" in response.data
+    # Verifique se um texto esperado está na página, ignorando maiúsculas/minúsculas
+    assert b"gerador de assinatura" in response.data.lower()
